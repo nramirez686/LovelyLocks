768e1a0022d563e4467081577eb597b17ac9d0f5
Fancy Nancy <ramireznancy.686@gmail.com>
2023-07-05 16:39:48 -0600

    updated password length function

--- script.js
+++ script.js
@@ -37,23 +37,36 @@ function generatePassword() {
   var numericChars = "0123456789";
   var specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
 
-  var availableChars = "";
-  if (includeLowercase) availableChars += lowercaseChars;
-  if (includeUppercase) availableChars += uppercaseChars;
-  if (includeNumeric) availableChars += numericChars;
-  if (includeSpecialChars) availableChars += specialChars;
+  var selectedChars = "";
+  if (includeLowercase) selectedChars += lowercaseChars;
+  if (includeUppercase) selectedChars += uppercaseChars;
+  if (includeNumeric) selectedChars += numericChars;
+  if (includeSpecialChars) selectedChars += specialChars;
 
   //for loop
   var password = "";
   for (var i = 0; i < passwordLength; i++) {
-    var randomIndex = Math.floor(Math.random() * availableChars.length);
-    password += availableChars.charAt(randomIndex);
+    var randomIndex = Math.floor(Math.random() * selectedChars.length);
+    password += selectedChars.charAt(randomIndex);
   }
 
   return password;
 }
 
-// function for password length and input
+/* Not working-
+function getPasswordLength() {
+  var passwordLengthString = prompt("Enter the desired password length (between 8 and 128 characters)");
+
+  if (!/^[8-9]|[1-9]\d|1[0-2]\d|128$/.test(passwordLengthString)) {
+    alert("Invalid password length. Enter a value between 8 and 128.");
+    return null;
+  }
+
+  return Number(passwordLengthString);
+}
+*/
+
+//function for password length and input
 function getPasswordLength() {
   var passwordLengthString = prompt(
     "Enter password length (between 8 and 128 characters)"
@@ -61,7 +74,7 @@ function getPasswordLength() {
   var passwordLength = parseInt(passwordLengthString);
 
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    alert("Invalid password length. Please enter a value between 8 and 128.");
+    alert("Invalid password length. Enter a value between 8 and 128.");
     return null;
   }
 
