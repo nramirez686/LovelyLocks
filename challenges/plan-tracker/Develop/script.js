814d74a6aede93a0827ed8d641f2f67539fd194c
Fancy Nancy <ramireznancy.686@gmail.com>
2023-07-26 18:23:21 -0600

    added readMe and functionality

--- challenges/plan-tracker/Develop/script.js
+++ challenges/plan-tracker/Develop/script.js
@@ -1,23 +1,51 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Function to display the current date
+function displayCurrentDate() {
+  const currentDate = new Date();
+  const day = currentDate.getDate();
+  const month = currentDate.toLocaleString("default", { month: "long" });
+  const year = currentDate.getFullYear();
+  const formattedDate = `${month} ${day}, ${year}`;
+  $("#currentDay span").text(formattedDate);
+}
+
+// Event listener for user input and saving to local storage
+$(document).on("click", ".saveBtn", function () {
+  const userInput = $(this).siblings(".description").val();
+  const timeBlockId = $(this).parent().attr("id");
+  const hourValue = timeBlockId.split("-")[1];
+  localStorage.setItem(hourValue, userInput);
+});
+
+// Function that applies the correct CSS to each block depending on the time
+function applyTimeBlockClasses() {
+  const currentHour = dayjs().hour();
+  $(".time-block").each(function () {
+    const timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+    if (timeBlockHour < currentHour) {
+      $(this).removeClass("present future").addClass("past");
+    } else if (timeBlockHour === currentHour) {
+      $(this).removeClass("past future").addClass("present");
+    } else {
+      $(this).removeClass("past present").addClass("future");
+    }
+  });
+}
+
+// Function that retrieves previously saved user input
+function getSavedUserInputs() {
+  $(".time-block").each(function () {
+    const timeBlockId = $(this).attr("id");
+    const userInput = localStorage.getItem(timeBlockId);
+
+    if (userInput) {
+      $(this).find(".description").val(userInput);
+    }
+  });
+}
+
+// Document ready function to execute the code when the DOM is loaded
+$(document).ready(function () {
+  displayCurrentDate();
+  applyTimeBlockClasses();
+  getSavedUserInputs();
 });
--- challenges/plan-tracker/README.md
+++ challenges/plan-tracker/README.md
@@ -1,1 +1,34 @@
 # Work Day Scheduler Starter Code
+
+## **Project Description**
+
+This was a challenge project in which I needed create a day scheduler using local storage
+
+## **Installation Instructions**
+
+To be able to access the project you won't need to install any new tools, just simply use your browser.
+
+## **How To Use The Project**
+
+The project required the following acceptance critiria
+-WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+-WHEN I scroll down
+THEN I am presented with time blocks for standard business hours of 9am to 5pm
+-WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+-WHEN I click into a time block
+THEN I can enter an event
+-WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+-WHEN I refresh the page
+THEN the saved events persist
+
+## **License**
+
+This project is released under the MIT License. Feel free to use the challenges and solutions provided here for personal or educational purposes.
+
+## **Deployed Website Link**
+
+https://nramirez686.github.io/ProfolioVibes/
+![Website screenshot](./assets/Screenshot%202023-07-26%20at%206.17.30%20PM.png)
